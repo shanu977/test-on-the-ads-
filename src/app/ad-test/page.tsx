8ee5bdd0d48d6afff1da757c25b0c3a642dbaaf5
shanu977 <pillishanu5@gmail.com>
2026-09-06 13:33:26 +0530

    fix(ad-test): correct script order to match Adsterra spec, ensure 1 script 1 container

--- src/app/ad-test/page.tsx
+++ src/app/ad-test/page.tsx
@@ -9,8 +9,8 @@ export default function AdTestPage() {
   return (
     <div style={{ maxWidth: 800, margin: "40px auto", padding: 20 }}>
       <h1 style={{ fontSize: 24, fontWeight: 700, marginBottom: 16 }}>Adsterra Test</h1>
+      <script async data-cfasync="false" src="https://pl31209749.profitableratecpmnetwork.com/839a36ce65a197c2f9ac39c8e70ca81e/invoke.js"></script>
       <div id="container-839a36ce65a197c2f9ac39c8e70ca81e"></div>
-      <script async={true} data-cfasync="false" src="https://pl31209749.profitableratecpmnetwork.com/839a36ce65a197c2f9ac39c8e70ca81e/invoke.js"></script>
     </div>
   );
 }
